87010edf16234efc1c92f222f7bab686797fdd0d
Reed Essick <reed.essick@ligo.org>
2018-01-10 22:39:31 -0600

    some work on investigate and supporting libraries

--- kde_inv/kde_inv.py
+++ kde_inv/kde_inv.py
@@ -3,13 +3,18 @@ __author__ = "Reed Essick (reed.essick@ligo.org)"
 
 #-------------------------------------------------
 
+import numpy as np
+
+#-------------------------------------------------
+
 def drawA(size=1):
     """
     draw samples from p(A)
 
     take p(A) = p(A[0])*p(A[1]) = 1
+    for A[0], A[1] \in [0, 1]
     """
-    raise NotImplementedError
+    return np.random.random(size=(2,size))
 
 def mapA2B(A):
     """
@@ -18,21 +23,37 @@ def mapA2B(A):
     take B = np.sin(2*np.pi*A[0])**2 * np.cos(4*np.pi*A[1])**2
     for A[0], A[1] \in [0, 1]
     """
-    raise NotImplementedError
+    return np.abs(np.sin(2*np.pi*A[0]) * np.cos(4*np.pi*A[1]))
 
-def optimizeBandwidth(samples):
+def optimizeBandwidth(samples, rtol=1e-6):
     """
     find the optimal bandwidth to describe samples with a Gaussian KDE via leave-one-out cross validation
 
     bisection search with starting boundary points as the closest distance between 2 samples and the furthest distance between 2 samples?
     """
-    raise NotImplementedError
+    max_bandwidth = np.max(samples) - np.min(samples)
+    order = samples.argsort()
+    min_samples = np.min(samples[order][1:]-samples[order][:-1])
+
+    raise NotImplementedError, 'implement a bisection search with truncation condition on relative improvement given by rtol'
 
 def leave1outLikelihood(samples, bandwidth):
     """
     determine the likelihood of observing samples with Gaussian KDE using bandwidth
     """
-    raise NotImplementedError
+    pdf = 1.0
+    truth = np.ones_like(samples, dtype=bool)
+    for i in xrange(len(samples)):
+        truth[i-1] = True
+        truth[i] = False
+        pdf *= kde_pdf(samples[i], samples[truth], bandwidth)
+    return pdf
+
+def kde_pdf(B, samples, bandwidth):
+    """
+    compute pdf at sample point B from KDE estimate of samples with bandwidth
+    """
+    return (2*np.pi*bandwidth)**-0.5 * np.sum(np.exp(-0.5*(B-samples)**2/bandwidth))/len(bandwidth)
 
 def kldiv(samples, foo):
     """
