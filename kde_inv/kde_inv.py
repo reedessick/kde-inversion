e844efb2d4058dd3ebfa617eb5264d16ca59874f
Reed Essick <reed.essick@ligo.org>
2018-01-15 21:21:24 -0600

    added explicit iteration over samples as a separate function call to approximate the logLikelihood. This seems to be MUCH faster than the other proceedure and give qualitatively similar results. It is now the default function used within kde_inv.kde_inv.likelihood_plot()

--- kde_inv/kde_inv.py
+++ kde_inv/kde_inv.py
@@ -69,11 +69,13 @@ def likelihood_plot(samples, bandwidths, num_test, num_trials, num_plot=101, ver
         if verbose:
             print('bandwidth=%.3e'%bandwidth)
             print('    computing logL')
-        logL, sigma_logL = logleave1outLikelihood(bandwidth, samples, num_test, num_trials, return_std=True)
+#        logL, sigma_logL = logleave1outLikelihood(bandwidth, samples, num_test, num_trials, return_std=True)
+        logL, sigma_logL = explicit_logleave1outLikelihood(bandwidth, samples, return_std=True)
 
         if verbose:
             print('    computing grad_logL')
-        gradlogL, sigma_gradlogL = grad_logleave1outLikelihood(bandwidth, samples, num_test, num_trials, return_std=True)
+#        gradlogL, sigma_gradlogL = grad_logleave1outLikelihood(bandwidth, samples, num_test, num_trials, return_std=True)
+        gradlogL, sigma_gradlogL = explicit_grad_logleave1outLikelihood(bandwidth, samples, return_std=True)
 
         if verbose:
             print('    plotting')
@@ -134,46 +136,19 @@ def logleave1outLikelihood(bandwidth, samples, num_test, num_trials=10, return_s
     else:
         return np.mean(loglike)
 
-'''
-### DEPRECATED: this is actually slower than logleave1outLikelihood for reasonable sample sizes
-def fast_logleave1outLikelihood(bandwidth, samples, num_test, num_trials=10):
+def explicit_logleave1outLikelihood(bandwidth, samples, return_std=False):
+    loglike = []
+    truth = np.ones_like(samples, dtype=bool)
     N = len(samples)
-    sample_inds = np.arange(N)
-    ones_N = np.ones_like(samples, dtype=float)
-    ones_T = np.ones(num_trials, dtype=float)
-
-    ### compute all kernals between samples
-    kernals = (2*np.pi*bandwidth)**-0.5 * np.exp(-0.5*(np.outer(samples, ones_N) - np.outer(ones_N, samples))**2/bandwidth)
-
-    ### randomly partition these into test and train sets
-    train = np.ones((num_trials,N), dtype=bool) ### shape : (num_trials, N)
-    inds = np.random.randint(0, N, size=num_trials*num_test).reshape((num_trials, num_test))
-    for k in xrange(num_trials):
-        train[k][inds[k]] = False ### select the subset for testing
-    num_train = np.outer(np.sum(train, axis=1), ones_N) ### shape : (num_trials, N)
-
-    test = np.logical_not(train)
-    num_test = np.sum(test, axis=1) ### shape : (num_trials,)
-
-    ### set up indicator function -> set only those elements that are included to non-zero values
-    training = np.zeros((num_trials, N, N), dtype=float) ### if not selected, this element should just be zero
-    for k in xrange(num_trials):
-        for i in sample_inds[test[k]]:
-            training[k][i][train[k]] = kernals[i][train[k]]
-
-    ### set up the summation over train samples
-    summed_over_train = np.log(np.sum(training, axis=2)) - np.log(num_train)
-
-    ### sum over only stuff in test set
-    for k in xrange(num_trials):
-        summed_over_train[k][train[k]] = 0 ### zero out the stuff that isn't in the test set
-    summed_over_test  = np.sum(summed_over_train, axis=1)/num_test
-
-    ### average over trials
-    summed_over_trials = np.sum(summed_over_test, axis=0)/num_trials
-
-    return -summed_over_trials ### return the negative because we want to minimize this...
-'''
+    n = N-1
+    for i in xrange(N):
+        truth[i-1] = True
+        truth[i] = False
+        loglike.append( -np.sum(logkde_pdf(samples[i], samples[truth], bandwidth))/n )
+    if return_std:
+        return np.mean(loglike), np.std(loglike)
+    else:
+        return np.mean(loglike)
 
 def grad_logleave1outLikelihood(bandwidth, samples, num_test, num_trials=10, return_std=False):
     """
@@ -195,11 +170,23 @@ def grad_logleave1outLikelihood(bandwidth, samples, num_test, num_trials=10, ret
     else:
         return np.mean(grad_loglike)
 
-def fast_grad_logleave1outLikelihood(bandwidth, samples, num_test, num_trials=10):
-    """
-    a version of grad_logleave2outLikelihood that uses numpy arrays to try to speed up the calculation
-    """
+def explicit_grad_logleave1outLikelihood(bandwidth, samples, return_std=False):
+    truth = np.ones_like(samples, dtype=bool)
+    N = len(samples)
+    n = N-1
+    grad_loglike = []
+    for i in xrange(N):
+        truth[i-1] = True
+        truth[i] = False
+        grad_loglike.append( -np.sum(grad_logkde_pdf(samples[i], samples[truth], bandwidth))/n )
+
+    if return_std:
+        return np.mean(grad_loglike), np.std(grad_loglike)
+    else:
+        return np.mean(grad_loglike)
     raise NotImplementedError
+
+#------------------------
 
 def logkde_pdf(B, samples, bandwidth):
     """
